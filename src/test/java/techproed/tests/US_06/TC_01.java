21cf5d1a64748bf75e83e1c745e11c185da592db
Sarah Frost Pedersen <sarahfrostpedersen@gmail.com>
2024-02-02 13:27:43 +0000

    Commit 020224_1327

---
+++ src/test/java/techproed/tests/US_06/TC_01.java
@@ -0,0 +1,30 @@
+package techproed.tests.US_06;
+
+public class TC_01 {
+
+    /*
+    Pre-condition:
+    User has signed in.
+
+    Click in the search box, enter product name and click enter
+    Click 'Add to Cart' button
+    Verify the message '"(product name)" has been added to your cart' appears
+    Click on Cart icon
+    Verify product is visible on Shopping Cart side bar
+    Click on View Cart button
+    Click on '+' button
+    Verify quantity amount has increased
+    Click on Update Cart button
+    Click on 'Proceed to Checkout' button
+    Verify country, street address, town, postcode and phone have been populated
+    Verify 'Payment Methods' are visible
+    Select 'Pay at the door'
+    Select 'Place order'
+    On order complete page verify 'Thank you. Your order has been received.' is visible
+     */
+
+
+
+
+
+}
---
+++ src/test/java/techproed/tests/US_06/TC_02.java
@@ -0,0 +1,17 @@
+package techproed.tests.US_06;
+
+public class TC_02 {
+
+    /*
+    Pre-condition:
+    User has signed in.
+
+
+    Click in the search box, enter random characters and click enter
+    Verify message 'No products were found matching your selection.' appears
+     */
+
+
+
+
+}
