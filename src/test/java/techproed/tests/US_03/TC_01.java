48adaaa3087ab08942dff4afd14ec8bf8b284192
Sarah Frost Pedersen <sarahfrostpedersen@gmail.com>
2024-02-02 17:04:52 +0000

    Commit 020224_1704

--- src/test/java/techproed/tests/US_03/TC_01.java
+++ src/test/java/techproed/tests/US_03/TC_01.java
@@ -21,5 +21,19 @@ public class TC_01 {
     Verify billing address has been added
     */
 
+//    User has just registered.
+//    User has already entered first name and last name under Account Details.
+//    User is on Billing address page (Home>My Account> Addresses>Billing address).
+//    Verify first name has been populated.
+//    Verify last name has been populated.
+//    Verify email address has been populated.
+//    From the country drop down list click on a valid country
+//    Click on street address and enter a valid street address for the chosen country
+//    Click on town/city and enter valid town name
+//    Click on Postcode and enter a valid postcode
+//    Click on Phone and enter a valid phone number
+//    Click on Save Address button
+//    Verify message 'Address changed successfully.' appears
+//    Verify billing address has been added
 
 }
--- src/test/java/techproed/tests/US_06/TC_02.java
+++ src/test/java/techproed/tests/US_06/TC_02.java
@@ -6,12 +6,13 @@ public class TC_02 {
     Pre-condition:
     User has signed in.
 
-
     Click in the search box, enter random characters and click enter
     Verify message 'No products were found matching your selection.' appears
      */
 
-
+//    User has signed in.
+//    Click in the search box, enter random characters and click enter
+//    Verify message 'No products were found matching your selection.' appears
 
 
 }
