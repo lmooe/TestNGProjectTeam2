4ae9060da778aa3524f6eff2c42f529151eb3bc4
Sarah Frost Pedersen <sarahfrostpedersen@gmail.com>
2024-02-04 13:35:48 +0000

    040224_1335

--- src/test/java/techproed/tests/US_06/TC_01.java
+++ src/test/java/techproed/tests/US_06/TC_01.java
@@ -1,14 +1,15 @@
 package techproed.tests.US_06;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Wait;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import techproed.pages.AddNewProductPage;
 import techproed.pages.HomePage;
 import techproed.pages.ProductPage;
-import techproed.utilities.BrowserUtils;
-import techproed.utilities.ConfigReader;
-import techproed.utilities.Driver;
+import techproed.utilities.*;
 
 public class TC_01 {
 
@@ -33,26 +34,39 @@ public class TC_01 {
     On order complete page verify 'Thank you. Your order has been received.' is visible
      */
 
-    @Test
-    public void TC_01(){
+    HomePage homePage = new HomePage();
+    ProductPage productPage = new ProductPage();
+
+    @BeforeMethod
+    public void setUp(){
 //    User is on www.allovercommerce.com
         Driver.getDriver().get(ConfigReader.getProperty("allovercom_url"));
+    }
+
+    @Test
+    public void TC_01(){
 //    User has signed in.
-        HomePage homePage = new HomePage();
-//        BrowserUtils.clickWithTimeOut(homePage.singin, 1);
-        BrowserUtils.clickWithTimeOut(Driver.getDriver().findElement(By.xpath("//*[text()='Sign In']")), 1);
-//        BrowserUtils.sendKeysWithTimeout(homePage.password, ConfigReader.getProperty("username"), 1);
-//        BrowserUtils.sendKeysWithTimeout(homePage.password, ConfigReader.getProperty("password"), 1);
-//        BrowserUtils.clickWithTimeOut(homePage.signButton, 1);
+        BrowserUtils.clickWithTimeOut(homePage.singIn1, 1);
+        Driver.getDriver().findElement(By.id("username")).click();
+        Driver.getDriver().findElement(By.id("username")).clear();
+        Driver.getDriver().findElement(By.id("username")).sendKeys(ConfigReader.getProperty("user1"));
+        Driver.getDriver().findElement(By.id("password")).click();
+        Driver.getDriver().findElement(By.id("password")).clear();
+        Driver.getDriver().findElement(By.id("password")).sendKeys(ConfigReader.getProperty("pass1"));
+        BrowserUtils.clickWithTimeOut(homePage.signInButton, 1);
+
+//        BrowserUtils.sendKeysWithTimeout(homePage.username, ConfigReader.getProperty("user1"), 1);
+//        BrowserUtils.sendKeysWithTimeout(homePage.password, ConfigReader.getProperty("pass1"), 1);
+//        BrowserUtils.clickWithTimeOut(homePage.signInButton, 1);
+
 //    Click in the search box, enter product name (Tshirt) and click enter
-        BrowserUtils.sendKeysWithTimeout(homePage.searchBox, "Tshirt", 1);
+//        BrowserUtils.sendKeysWithTimeout(homePage.searchBox, "Tshirt", 1);
 //    Searchbox locator //input[@class='form-control']
 //    Click 'Add to Cart' button
-        ProductPage productPage = new ProductPage();
-        BrowserUtils.clickWithTimeOut(productPage.addToCartButton, 1);
+//        BrowserUtils.clickWithTimeOut(productPage.addToCartButton, 1);
 //    Verify the message "'Tshirt' has been added to your cart." appears
-        String successMessage = productPage.addedToCartAlert.getText();
-        Assert.assertEquals(successMessage,"VIEW CART \"Tshirt\" has been added to your cart.");
+//        String successMessage = productPage.addedToCartAlert.getText();
+//        Assert.assertEquals(successMessage,"VIEW CART \"Tshirt\" has been added to your cart.");
 //    Click on Cart icon
 //    Verify product is visible on Shopping Cart sidebar
 //    Click on View Cart button
