d63341b13a3216b386449cb56973aada1eb3d925
Sloane <sloanealltree@gmail.com>
2024-02-05 11:35:47 -0500

    first group commit

--- src/test/java/techproed/pages/Bill_ShipAddressPage.java
+++ src/test/java/techproed/pages/Bill_ShipAddressPage.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import techproed.utilities.Driver;
 
-public class Billing_ShippingPage {
+public class Bill_ShipAddressPage {
 
-    public Billing_ShippingPage(){
+    public Bill_ShipAddressPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
