f4e2a8c53fdea5579c79625dad923f73078c5993
Fatih Tamer <fatihtamer1992@yahoo.com>
2024-02-05 10:56:56 -0500

    20240205_1056

--- src/test/java/techproed/pages/DashboardPage.java
+++ src/test/java/techproed/pages/DashboardPage.java
@@ -3,10 +3,14 @@ package techproed.pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import techproed.utilities.Driver;
 
 public class DashboardPage {
 
-
+    public DashboardPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 
     @FindBy(xpath = "//a[text()='Dashboard']")
     public WebElement dashboard;
