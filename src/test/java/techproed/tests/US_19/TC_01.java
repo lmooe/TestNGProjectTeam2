896db8e85fe480c03408b36b95172fbae054c9e0
Ozzie Akdemir <5ozleme5@gmail.com>
2024-02-08 19:47:01 +0000

    10

--- src/test/java/techproed/tests/US_19/TC_01.java
+++ src/test/java/techproed/tests/US_19/TC_01.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import techproed.pages.*;
@@ -27,53 +28,71 @@ public class TC_01 {
     CouponPage couponpage = new CouponPage();
     ExtentReportUtils extentreportutils = new ExtentReportUtils();
 
+    @BeforeMethod
+    public void deleteCart() {
+        Driver.getDriver().get(ConfigReader.getProperty("allovercom_url"));
 
-      @Test
-       public void test01() {
+        //Sign in
+        JSUtils.JSclickWithTimeout(homepage.singIn1);
+        homepage.username.sendKeys(ConfigReader.getProperty("email"));
+        homepage.password.sendKeys(ConfigReader.getProperty("password"));
+        homepage.signInButton.click();
+        WaitUtils.waitFor(4);
 
-          Driver.getDriver().get(ConfigReader.getProperty("allovercom_url"));
-          homepage.singIn1.click();
-          homepage.username.sendKeys(ConfigReader.getProperty("email"));
-          homepage.password.sendKeys(ConfigReader.getProperty("password"));
-          homepage.signInButton.click();
-          WaitUtils.waitFor(2);
+        //Delete cart, if it is not empty
+        if (Integer.parseInt(checkoutPage.productCounter.getText()) > 0) {
+            homepage.cart.click();
+            checkoutPage.viewChartButton.click();
+            checkoutPage.clearChart.click();
+        }
+    }
+        @Test
+        public void test01() {
+
+         //   Driver.getDriver().get(ConfigReader.getProperty("allovercom_url"));
+          //  homepage.singIn1.click();
+           // homepage.username.sendKeys(ConfigReader.getProperty("email"));
+           // homepage.password.sendKeys(ConfigReader.getProperty("password"));
+          //  homepage.signInButton.click();
+           // WaitUtils.waitFor(2);
 
 
-          homepage.searchBox.sendKeys(ConfigReader.getProperty("product0"), Keys.ENTER);
-          WebElement womenBag = Driver.getDriver().findElement(By.linkText("Women’s leather white hangbag"));
-          JSUtils.JSclickWithTimeout(womenBag);
+            homepage.searchBox.sendKeys(ConfigReader.getProperty("product0"), Keys.ENTER);
+            WebElement womenBag = Driver.getDriver().findElement(By.linkText("Women’s leather white hangbag"));
+            JSUtils.JSclickWithTimeout(womenBag);
 
-          WaitUtils.waitFor(1);
+            WaitUtils.waitFor(1);
 
-          for (int i = 1; i < 4; i++) {
-              productPage.plusButton.click();
-          }
-          productPage.addToCartButton2.click();
-          WaitUtils.waitFor(1);
+            for (int i = 1; i < 4; i++) {
+                productPage.plusButton.click();
+            }
+            productPage.addToCartButton2.click();
+            WaitUtils.waitFor(1);
 
-          homepage.cart.click();
-          WaitUtils.waitFor(1);
+            homepage.cart.click();
+            WaitUtils.waitFor(1);
 
-          WebElement checkOut = Driver.getDriver().findElement(By.xpath("//a[@class='button checkout wc-forward']"));
-          JSUtils.JSclickWithTimeout(checkOut);
+            WebElement checkOut = Driver.getDriver().findElement(By.xpath("//a[@class='button checkout wc-forward']"));
+            JSUtils.JSclickWithTimeout(checkOut);
 
-          checkoutPage.enterYourCode.click();
-          WaitUtils.waitFor(1);
-          checkoutPage.couponCodeField.click();
-          checkoutPage.couponCodeField.sendKeys(ConfigReader.getProperty("code"));
-          WaitUtils.waitFor(1);
-          checkoutPage.applyCouponButton.click();
-          WaitUtils.waitFor(3);
+            checkoutPage.enterYourCode.click();
+            WaitUtils.waitFor(1);
+            checkoutPage.couponCodeField.click();
+            checkoutPage.couponCodeField.sendKeys(ConfigReader.getProperty("code"));
+            WaitUtils.waitFor(1);
+            checkoutPage.applyCouponButton.click();
+            WaitUtils.waitFor(3);
 
-          ExtentReportUtils.createTestReport("test01", "fail");
-          ExtentReportUtils.passAndCaptureScreenshot("Sorry, this coupon is not applicable to selected products");
-          ExtentReportUtils.flush();
+            ExtentReportUtils.createTestReport("test01", "fail");
+            ExtentReportUtils.passAndCaptureScreenshot("Sorry, this coupon is not applicable to selected products");
+            ExtentReportUtils.flush();
 
-          Driver.closeDriver();
-
-      }
+            Driver.closeDriver();
 
         }
+
+    }
+
 
 
 
